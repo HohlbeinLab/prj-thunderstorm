abc595cdf2174d978a20333c28289a61190d8aa5
davit.ninoshvili <davit.ninoshvili@quantori.com>
2022-09-30 18:43:11 +0400

    Update deprecated LevenbergMarquardtOptimizer in LSQFitter

--- src/main/java/cz/cuni/lf1/lge/ThunderSTORM/estimators/LSQFitter.java
+++ src/main/java/cz/cuni/lf1/lge/ThunderSTORM/estimators/LSQFitter.java
@@ -4,35 +4,33 @@ import cz.cuni.lf1.lge.ThunderSTORM.estimators.PSF.Molecule;
 import cz.cuni.lf1.lge.ThunderSTORM.estimators.PSF.PSFModel;
 import cz.cuni.lf1.lge.ThunderSTORM.estimators.PSF.PSFModel.Params;
 import cz.cuni.lf1.lge.ThunderSTORM.util.VectorMath;
-import org.apache.commons.math3.optim.*;
-import org.apache.commons.math3.optim.nonlinear.vector.ModelFunction;
-import org.apache.commons.math3.optim.nonlinear.vector.ModelFunctionJacobian;
-import org.apache.commons.math3.optim.nonlinear.vector.Target;
-import org.apache.commons.math3.optim.nonlinear.vector.Weight;
-import org.apache.commons.math3.optim.nonlinear.vector.jacobian.LevenbergMarquardtOptimizer;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresBuilder;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresProblem;
+import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
+import org.apache.commons.math3.linear.DiagonalMatrix;
 
 import static cz.cuni.lf1.lge.ThunderSTORM.util.VectorMath.sub;
 
 public class LSQFitter implements IOneLocationFitter, IOneLocationBiplaneFitter {
-
     public final static int MAX_ITERATIONS = 1000;
 
     public double[] fittedParameters;
     public PSFModel psfModel;
     public boolean useWeighting;
 
-    private int maxIter;    // after `maxIter` iterations the algorithm converges
-    private int bkgStdColumn;
+    private final int maxIter; // after `maxIter` iterations the algorithm converges
+    private final int bkgStdColumn;
 
     public LSQFitter(PSFModel psfModel, boolean useWeighting) {
-        this(psfModel, useWeighting, MAX_ITERATIONS + 1, -1 );
-    }
-    
-    public LSQFitter(PSFModel psfModel, boolean useWeighting, int bkgStdIndex) {
-        this(psfModel, useWeighting, MAX_ITERATIONS + 1, Params.BACKGROUND );
+        this(psfModel, useWeighting, MAX_ITERATIONS + 1, -1);
     }
 
-    public LSQFitter(PSFModel psfModel, boolean useWeighting, int maxIter, int bkgStdIndex) {// throws an exception after `MAX_ITERATIONS` iterations
+    public LSQFitter(PSFModel psfModel, boolean useWeighting, int bkgStdIndex) {
+        this(psfModel, useWeighting, MAX_ITERATIONS + 1, Params.BACKGROUND);
+    }
+
+    public LSQFitter(PSFModel psfModel, boolean useWeighting, int maxIter, int bkgStdIndex) { // throws an exception after `MAX_ITERATIONS` iterations
         this.psfModel = psfModel;
         this.maxIter = maxIter;
         this.useWeighting = useWeighting;
@@ -56,21 +54,24 @@ public class LSQFitter implements IOneLocationFitter, IOneLocationBiplaneFitter 
         double[] observations = functions.getObservations();
 
         // fit
-        LevenbergMarquardtOptimizer optimizer = new LevenbergMarquardtOptimizer(
-                new SimplePointChecker<PointVectorValuePair>(10e-10, 10e-10, maxIter));
+        LeastSquaresOptimizer optimizer = new LevenbergMarquardtOptimizer()
+                .withCostRelativeTolerance(1.0e-2)
+                .withParameterRelativeTolerance(1.0e-2);
 
-        PointVectorValuePair pv;
-        pv = optimizer.optimize(
-                MaxEval.unlimited(),
-                new MaxIter(MAX_ITERATIONS + 1),
-                new ModelFunction(functions.getValueFunction()),
-                new ModelFunctionJacobian(functions.getJacobianFunction()),
-                new Target(observations),
-                new InitialGuess(psfModel.transformParametersInverse(functions.getInitialParams())),
-                new Weight(weights));
+        LeastSquaresProblem leastSquaresProblem = new LeastSquaresBuilder()
+                .start(psfModel.transformParametersInverse(functions.getInitialParams()))
+                .target(observations)
+                .model(functions.getValueFunction(), functions.getJacobianFunction())
+                .lazyEvaluation(false)
+                .maxEvaluations(Integer.MAX_VALUE)
+                .maxIterations(maxIter)
+                .weight(new DiagonalMatrix(weights))
+                .build();
+
+        LeastSquaresOptimizer.Optimum optimum = optimizer.optimize(leastSquaresProblem);
 
         // estimate background and return an instance of the `Molecule`
-        fittedParameters = pv.getPointRef();
+        fittedParameters = optimum.getPoint().toArray();
         if (bkgStdColumn >= 0) {
             fittedParameters[bkgStdColumn] = VectorMath.stddev(sub(observations, functions.getValueFunction().value(fittedParameters)));
         }
